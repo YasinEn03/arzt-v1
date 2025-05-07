942749e51911ad58f8d65bd034e533c5fb229d01
Yasin Ayyildiz <ayyildiz.yasinenes03@gmail.com>
2025-05-07 21:10:50 +0200

    test adjusting

--- src/config/node.ts
+++ src/config/node.ts
@@ -1,18 +1,3 @@
-// Copyright (C) 2016 - present Juergen Zimmermann, Hochschule Karlsruhe
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program. If not, see <https://www.gnu.org/licenses/>.
-
 /**
  * Das Modul enthält die Konfiguration für den _Node_-basierten Server.
  * @packageDocumentation
@@ -34,24 +19,10 @@ if (
 ) {
     throw new TypeError('Der konfigurierte Port ist keine Zahl');
 }
-// "Optional Chaining" und "Nullish Coalescing" ab ES2020
 const port = (node?.port as number | undefined) ?? 3000; // eslint-disable-line @typescript-eslint/no-magic-numbers
 
-/**
- * Die Konfiguration für den _Node_-basierten Server:
- * - Rechnername
- * - IP-Adresse
- * - Port
- * - `PEM`- und Zertifikat-Datei mit dem öffentlichen und privaten Schlüssel
- *   für TLS
- */
-// "as const" fuer readonly
-// https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-4.html#const-assertions
-// TODO records als "deeply immutable data structure" (Stage 2)
-// https://github.com/tc39/proposal-record-tuple
 export const nodeConfig = {
     host: computername,
-    // Shorthand Property ab ES 2015
     port,
     resourcesDir: RESOURCES_DIR,
     httpsOptions,
