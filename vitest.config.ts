160091773020a28fe0b9e4274818f62e48d1bb66
Yasin Ayyildiz <141184289+YasinEn03@users.noreply.github.com>
2025-05-07 19:38:38 +0200

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,18 +2,18 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    test: {
-        name: 'Beispiel',
-        include: ['test/**/*.{test,spec}.{ts,js,mts}'],
-        globals: true,
-        environment: 'node',
-        globalSetup: './test/setup.global.ts',
-        testTimeout: 10_000,
-        coverage: {
-            include: ['src/**', 'test/**'],
-            exclude: ['src/config/resources/**'],
-            extension: ['.mts', '.ts'],
-            reporter: ['text', 'html'],
-        },
+  test: {
+    name: 'Beispiel',
+    include: ['test/**/*.test.{ts,js,mts}', 'test/**/*.spec.{ts,js,mts}'],
+    globals: true,
+    environment: 'node',
+    globalSetup: './test/setup.global.ts',
+    testTimeout: 10_000,
+    coverage: {
+      include: ['src/**', 'test/**'],
+      exclude: ['src/config/resources/**'],
+      extension: ['.mts', '.ts'],
+      reporter: ['text', 'html'],
     },
+  },
 });
