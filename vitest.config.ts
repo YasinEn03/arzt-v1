1d20b30701dbcd176e3001f3f2518989fa47c541
Yasin Ayyildiz <ayyildiz.yasinenes03@gmail.com>
2025-05-07 13:30:54 +0200

    config change

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,27 +1,19 @@
 /// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
 
-// https://vitest.dev/config
 export default defineConfig({
     test: {
         name: 'Beispiel',
-        // default ist ['**\/*.{test,spec}.?(c|m)[jt]s?(x)']
-        include: ['test/**/*.test.mts'],
+        include: ['test/**/*.{test,spec}.mts'],
         globals: true,
         environment: 'node',
-        // https://vitest.dev/config/#globalsetup
         globalSetup: './test/setup.global.mts',
         testTimeout: 10_000,
-        // https://vitest.dev/guide/coverage
-        // https://vitest.dev/config/#coverage
         coverage: {
-            include: ['src/**'],
+            include: ['src/**', 'test/**'],
             exclude: ['src/config/resources/**'],
             extension: ['.mts', '.ts'],
-            // default ist ['text', 'html', 'clover', 'json']
             reporter: ['text', 'html'],
-            // default ist 'v8'
-            // provider: 'istanbul',
         },
     },
 });
